c2670a3f0552268b3413ebe8094918661c3b6b0b
emadunan <emadunan@gmail.com>
2022-04-10 15:52:46 +0200

    edit circleci configurations

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,1 +1,1 @@
-eb init --region us-east-1 --platform 'Node.js 16 running on 64bit Amazon Linux 2' udagram-api && eb use udagram-api-dev && eb deploy
+cd www && eb init --region us-east-1 --platform 'Node.js 16 running on 64bit Amazon Linux 2' udagram-api && eb use udagram-api-dev && eb deploy
